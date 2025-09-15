f9c70fbe4bfb30c3832d057b1a7d83ac91d37577
Riley <rileycarr2005@gmail.com>
2025-09-15 14:42:55 -0500

    solved problem 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if (typeof firstNum !== 'number' || typeof secondNum !== 'number') { // Detects if values are numbers
+        return 'ERROR';
+    } else if (firstNum < 0 || secondNum < 0) { // Detects if negative
+        return 'ERROR';
+    } else if (firstNum % 1 !== 0 || secondNum % 1 !== 0) { // Detects if not an integer
+        return 'ERROR';
+    } else if (firstNum > secondNum) {
+        startVal = secondNum;
+        endVal = firstNum;
+    } else {
+        startVal = firstNum;
+        endVal = secondNum;
+    }
 
+    let sum = 0;
+    for (let i = startVal; i <= endVal; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
