77fc1a01e8d63e60cafea6e6ca3e26d7f3a3b5c2
Riley <rileycarr2005@gmail.com>
2025-09-15 12:36:45 -0500

    fixed 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,6 @@
 const reverseString = function(stringToReverse) {
     let newString = '';
-    for (let i = length(stringToReverse) - 1; i >= 0; i--) {
+    for (let i = stringToReverse.length - 1; i >= 0; i--) {
         newString += stringToReverse[i];
     }
     return newString;
